0a8a4dbe8b77613f221c60c65ffad75662337ea5
lamanhdai <lanhdai1988@gmail.com>
2018-05-06 22:49:41 +0700

    d3

--- population-chart/src/app/app.module.ts
+++ population-chart/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppComponent } from './app.component';
 import { ChartComponent } from './chart/chart.component';
 import { SearchComponent } from './search/search.component';
--- population-chart/src/app/chart/chart.component.ts
+++ population-chart/src/app/chart/chart.component.ts
@@ -1,22 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import * as d3 from 'd3';
 
 import { Chart } from '../chart';
 import { ChartService } from '../chart.service';
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
-  styleUrls: ['./chart.component.scss']
+  styleUrls: ['./chart.component.scss'],
 })
 export class ChartComponent implements OnInit {
- 
   selectedChart: Chart;
-
   charts: Chart[];
+
+  width = 960;
+  height = 500;
 
   constructor(private chartService: ChartService) { }
   
   ngOnInit() {
     this.getCharts();
+    this.generateMap();
   }
 
   onSelect(chart: Chart): void {
@@ -28,4 +31,20 @@ export class ChartComponent implements OnInit {
       .subscribe(charts => this.charts = charts);
   }
 
+  // d3 map
+  private generateMap() {
+    const svg = d3.select("svg");
+    
+    const projection = d3.geoMercator()
+      .scale(this.width / 2 / Math.PI)
+      //.scale(100)
+      .translate([this.width / 2, this.height / 2])
+
+    const path = d3.geoPath()
+      .projection(projection);
+
+    d3.json('./assets/world-110m.json', (err: any, geojson: any) => {
+      svg.append("path").attr("d", path(geojson))
+    })
+  }
 }
