e28284b979de887614df3a4dcda3d36f335cc44c
lamanhdai <lanhdai1988@gmail.com>
2018-05-09 23:43:20 +0700

    feat(d3): chart

--- population-chart/src/app/app.module.ts
+++ population-chart/src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { D3Service, D3_DIRECTIVES } from './d3';
+
 import { AppComponent } from './app.component';
 import { ChartComponent } from './chart/chart.component';
 import { SearchComponent } from './search/search.component';
 import { ChartService } from './chart.service';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
+import { GraphComponent } from './visuals/graph/graph.component';
+import { SHARED_VISUALS } from './visuals/shared';
 
 
 @NgModule({
--- population-chart/src/app/chart/chart.component.ts
+++ population-chart/src/app/chart/chart.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import * as d3 from 'd3';
 
 import { Chart } from '../chart';
 import { ChartService } from '../chart.service';
@@ -12,14 +11,10 @@ export class ChartComponent implements OnInit {
   selectedChart: Chart;
   charts: Chart[];
 
-  width = 960;
-  height = 500;
-
   constructor(private chartService: ChartService) { }
   
   ngOnInit() {
     this.getCharts();
-    this.generateMap();
   }
 
   onSelect(chart: Chart): void {
@@ -29,22 +24,5 @@ export class ChartComponent implements OnInit {
   getCharts(): void {
     this.chartService.getCharts()
       .subscribe(charts => this.charts = charts);
-  }
-
-  // d3 map
-  private generateMap() {
-    const svg = d3.select("svg");
-    
-    const projection = d3.geoMercator()
-      .scale(this.width / 2 / Math.PI)
-      //.scale(100)
-      .translate([this.width / 2, this.height / 2])
-
-    const path = d3.geoPath()
-      .projection(projection);
-
-    d3.json('./assets/world-110m.json', (err: any, geojson: any) => {
-      svg.append("path").attr("d", path(geojson))
-    })
   }
 }
