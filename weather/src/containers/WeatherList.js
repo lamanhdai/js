c80bd88dc698a586fc941fe829803375a4498f53
Anh Dai <lanhdai1988@gmail.com>
2018-05-27 21:54:50 +0700

    add map google

--- weather/src/containers/WeatherList.js
+++ weather/src/containers/WeatherList.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
+import Chart from '../components/chart'
+import GoogleMap from '../components/GoogleMap'
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const cityName = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const pressus = cityData.list.map(weather => weather.main.pressure);
+    const hums = cityData.list.map(weather => weather.main.humidity);
+    const {lon, lat} = cityData.city.coord
+
     return (
       <tr key={cityName}>
-        <td>{cityName}</td>
+        <td>
+          <GoogleMap className="googlemap" lon={lon} lat={lat} />
+        </td>
+        <td>
+          <Chart data={temps} color="red" />
+        </td>
+        <td>
+          <Chart data={pressus} color="blue" />
+        </td>
+        <td>
+          <Chart data={hums} color="grey" />
+        </td>
       </tr>
     )
   }
--- weather/src/reducers/WeatherReducer.js
+++ weather/src/reducers/WeatherReducer.js
@@ -1,7 +1,7 @@
 import {FETCH_WEATHER} from '../actions'
 export default function(state = [], action) {
   switch(action.type) {
-    case FETCH_WEATHER: return [action.payload.data, ...state];
+    case FETCH_WEATHER: return action.payload.data ? [action.payload.data, ...state] : [...state];
   }
   return state;
 }
