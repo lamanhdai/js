a6fbded832572f2f8d85ec4a39efa916c1d161a0
Anh Dai <lanhdai1988@gmail.com>
2018-06-12 23:24:17 +0700

    delete action api

--- ReactReduxRouter/src/components/PostsShow.js
+++ ReactReduxRouter/src/components/PostsShow.js
@@ -1,11 +1,34 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import PropTypes from 'prop-types'
+import {fetchPost, deletePost} from '../actions'
+import {Link} from 'react-router-dom'
 
 class PostsShow extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  }
+  componentDidMount() {
+    this.props.fetchPost(this.props.match.params.id)
+  }
+  onDelete = () => {
+    this.props.deletePost(this.props.match.params.id).then(() => {
+      this.context.router.history.push('/posts')
+    })
+  }
   render() {
-    return <div>{this.props.match.params.id}</div>
+    const {post} = this.props;
+    if(!this.props.post) return <div>Loading...</div>
+    return <div>
+      <Link to="/posts">Back to Index</Link>
+      <button className="btn btn-danger pull-xs-right" onClick={this.onDelete}>Delete Post</button>
+      <h3>{post.title}</h3>
+      <h6>{post.categories}</h6>
+      <p>{post.content}</p>
+    </div>
   }
 }
-
-// const PostsShow = ({match}) =>  <div>{match.params.id}</div>
-
-export default PostsShow
+function mapStateToProps (state) {
+  return { post: state.posts.post }
+}
+export default connect(mapStateToProps, {fetchPost, deletePost})(PostsShow)
--- ReactReduxRouter/src/reducers/reducer_posts.js
+++ ReactReduxRouter/src/reducers/reducer_posts.js
@@ -1,9 +1,10 @@
-import {FETCH_POSTS, CREATE_POSTS} from '../actions';
+import {FETCH_POSTS, FETCH_POST} from '../actions';
 
 const INITIAL_STATE = { all: [], post: null};
 
 export default function(state=INITIAL_STATE, action) {
   switch(action.type) {
+    case FETCH_POST: return { ...state, post: action.payload.data };
     case FETCH_POSTS: return {...state, all: action.payload.data};
     default: return state;
   }
