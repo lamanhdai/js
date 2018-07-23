fb29df844fa59068d00d26cbdfa8727662879cdc
Anh Dai LAM <anh-dai.lam@ekino.com>
2018-07-23 18:03:12 +0700

    feat(saga): simple weather redux saga

---
+++ react-redux-saga-weather/src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
+import logo from './logo.svg';
+import './App.css';
+import reducer from './reducers';
+import mySaga from './sagas';
+
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware();
+
+// mount it on the Store
+const store = createStore(
+  reducer,
+  applyMiddleware(sagaMiddleware)
+)
+
+// then run the saga
+sagaMiddleware.run(mySaga)
+
+// render the application
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
