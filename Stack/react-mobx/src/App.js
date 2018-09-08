ed6b5cb981bcdd9fa584b1cb17e476acb33756ba
Anh Dai <lanhdai1988@gmail.com>
2018-09-09 00:24:07 +0700

    mobx

--- Stack/react-mobx/src/App.js
+++ Stack/react-mobx/src/App.js
@@ -1,22 +1,21 @@
 import React, { Component } from 'react';
-import {observer} from 'mobx-react';
-import {observable} from 'mobx';
-import logo from './logo.svg';
+
+import ObservableTodoStore from './Components/TodoStore';
 import './App.css';
 
-@observer
 export default class App extends Component {
-  @observable boxVisible = true;
+  renderTodo() {
+    ObservableTodoStore.addTodo('read Mobx concept');
+    ObservableTodoStore.addTodo('try Mobx');
+    ObservableTodoStore.todos[0].completed = true;
+    ObservableTodoStore.todos[0].task = 'challenge Mobx';
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {
+          this.renderTodo()
+        }
       </div>
     );
   }
---
+++ Stack/react-mobx/src/Components/TodoStore.js
@@ -0,0 +1,31 @@
+import {observable, computed} from 'mobx';
+
+class ObservableTodoStore {
+  @observable todos = [];
+
+  constructor() {
+    mobx.autorun(() => console.log(this.report));
+  }
+
+  @computed get completedTodosCount() {
+    return this.todos.filter(
+      todo => todo.completed === true
+    ).length;
+  }
+
+  @computed get report() {
+    if (!this.todos.length)
+      return '<none>';
+    return `Next todo: "${this.todos[0].task}". Progress: ${this.completedTodosCount}/${this.todos.length}`
+  }
+
+  addTodo(task) {
+    this.todos.push({
+      task: task,
+      completed: false,
+      assignee: null
+    });
+  }
+}
+
+export default ObservableTodoStore;
